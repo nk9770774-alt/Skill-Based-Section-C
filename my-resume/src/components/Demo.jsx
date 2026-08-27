6faa47977ca49a93ca3fcce5e5dd605d038cba9b
Nitin singh <nk9770774@gmail.com>
2026-08-27 09:03:53 +0530

    css added

---
+++ my-resume/src/components/Demo.jsx
@@ -0,0 +1,10 @@
+function Demo(){
+    return (
+        <>
+        <h1>Nitin Singh</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae maxime soluta molestiae ratione, maiores suscipit amet quo, optio, delectus architecto quisquam eius deleniti incidunt itaque dicta dolorem nobis explicabo. Doloribus voluptatibus ab veritatis maiores debitis dolores ipsam fugit enim.</p>
+        </>
+
+    );
+}
+export default Demo;
